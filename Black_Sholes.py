d02f1048bc5afa9eb82fa1c55c38f65fb894c7d2
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2022-06-26 17:02:23 +0200

    Mode documentation for black_sholes

--- Black_Sholes.py
+++ Black_Sholes.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from typing import Any
 
-# This method generates a Weiner process (more commonly known as a Brownian Motion)
 def generate_weiner_process(x0: float= 1, T: int = 1, dt: float = 0.001, rho: float = None) -> Any:
     # GENERATE_WEINER_PROCESS calculates the sample paths of a one-dimensional Brownian motion or a two-dimensional Brownian motion with a correlation coefficient of rho.
     # The function's output are two sample paths (realisations) of such a process, recorded on increments specified by dt. 
@@ -65,10 +64,9 @@ def generate_weiner_process(x0: float= 1, T: int = 1, dt: float = 0.001, rho: fl
 
         return [W_1, W_2]
 
-# This simulates a temporal series of stock prices using the Black Scholes log normal model and the generated Weiner process
 def simulate_Black_Scholes(x0: float= 1, S0: int = 100, mu: float = 0.05, sigma: float = 0.3, T: int = 52, dt: float = 0.1, rho: float = None) -> pd.DataFrame:
     # SIMULATE_BLACK_SHOLES calculates a temporal series of stock prices using the Black Scholes log normal model and the generated Brownian motion
-    # stock_price_simulation = simulate_Black_Scholes(self, S0, mu, sigma, T, dt, rho)
+    # stock_price_simulation = simulate_Black_Scholes(x0, S0, mu, sigma, T, dt, rho)
     #
     # Arguments:
     #   x0   =  float, the starting value of the Brownian motion
@@ -83,14 +81,36 @@ def simulate_Black_Scholes(x0: float= 1, S0: int = 100, mu: float = 0.05, sigma:
     #   stock_price_simulation = N x 2 pandas DataFrame where index is modeling time and values are a realisation of the uderlying's price
     #
     # Example:
+    #   Model the price of a stock whitch is worth today 100. The market has a future annualized risk free rate of 5% and an annualized volatility of 30%. The user is interested in a price projection for the next 10 years in increments of 6 months (0.5 years)
     #   import pandas as pd
     #   import numpy as np
-    #   ToFinish: simulate_Black_Scholes(1, 100,0.05,0.3, 4,0.5,None)   
-    #   [out] =  pd array    
+    #   from typing import Any
+    #   simulate_Black_Scholes(1, 100, 0.05, 0.3, 10,0.5, None)   
+    #   [out] = Time    Stock Price                
+    #           0.000000    134.985881
+    #           0.526316    130.306622
+    #           1.052632    122.687338
+    #           1.578947    103.717078
+    #           2.105263    109.418052
+    #           2.631579     88.875926
+    #           3.157895    112.805021
+    #           3.684211    119.405081
+    #           4.210526    159.967595
+    #           4.736842    160.401897
+    #           5.263158    169.804621
+    #           5.789474    167.541434
+    #           6.315789    192.277426
+    #           6.842105    193.039165
+    #           7.368421    177.318946
+    #           7.894737    182.171283
+    #           8.421053    139.995595
+    #           8.947368    189.266738
+    #           9.473684    200.767017
+    #           10.000000   182.350764    
     #
     # For more information see SOURCE
     
-    N = int(T / dt)
+    N = int(T / dt) # number of subintervals of length 1/dt between 0 and max modeling time T
 
     time, delta_t = np.linspace(0, T, num = N, retstep = True)
 
@@ -98,7 +118,7 @@ def simulate_Black_Scholes(x0: float= 1, S0: int = 100, mu: float = 0.05, sigma:
 
     weiner_process = sigma * generate_weiner_process(x0, T, dt)
 
-    S = S0*(np.exp(stock_variation + weiner_process))
+    S = S0*(np.exp(stock_variation + weiner_process)) # Change in price of the stock in time
 
     dict = {'Time' : time, 'Stock Price' : S}
 
@@ -106,5 +126,3 @@ def simulate_Black_Scholes(x0: float= 1, S0: int = 100, mu: float = 0.05, sigma:
     stock_price_simulation.set_index('Time', inplace = True)
 
     return stock_price_simulation
-
-print(simulate_Black_Scholes( 1, 100, 0.05, 0.3, 52,0.1, None))
